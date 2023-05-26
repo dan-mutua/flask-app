f7d0f2af196dc2dfcfce9f0aefae44fc88258a7d
dan-mutua <danmutua15@gmail.com>
2023-05-27 01:54:23 +0300

    create ec2 in terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+resource "aws_instance" "flask-app" {
+  ami              = "ami-053b0d53c279acc90"
+  instance_type    = "t2.medium"
+  security_groups = ["launch-wizard-4"]
+  key_name         = "mrl"
+  
+  root_block_device {
+    volume_size = 40
+  }
+  
+  monitoring = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+
+    # Install Docker
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+
+    # Install Minikube dependencies
+    sudo apt-get install -y curl
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+    # Start Minikube
+    sudo minikube start --driver=docker --kubernetes-version=stable
+
+    # Set up kubectl
+    sudo apt-get install -y kubectl
+
+    # Enable addons (optional)
+    sudo minikube addons enable ingress
+
+    # Set up kubeconfig for the current user
+    mkdir -p $HOME/.kube
+    sudo cp -i /root/.kube/config $HOME/.kube/
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+   
+  EOF
+}
